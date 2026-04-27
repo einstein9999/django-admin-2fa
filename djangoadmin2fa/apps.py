67546f05399aa021c26c68ce3b7baad8433b177a
einstein9999 <153386061+einstein9999@users.noreply.github.com>
2026-04-27 11:00:34 +0300

    clean: edited module verbose name

--- djangoadmin2fa/apps.py
+++ djangoadmin2fa/apps.py
@@ -5,5 +5,5 @@ class Admin2FaConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = 'djangoadmin2fa'
     label = 'djangoadmin2fa'
-    verbose_name = "Admin 2FA by HIMOSOFT" 
+    verbose_name = "Admin 2FA" 
     
